f8b7b4675c3486e672953767c5b4b4bd78daa8c5
gjmarz <61209817+gjmarz@users.noreply.github.com>
2020-02-18 16:08:45 -0500

    Update schemas_HelloFresh.sql

--- schemas_HelloFresh.sql
+++ schemas_HelloFresh.sql
@@ -3,16 +3,18 @@ USE John_Smith;
 
 
 DROP TABLE IF EXISTS subscription;
-CREATE TABLE subscription (
+CREATE TABLE subscription 
+(
   id_subscription INT AUTO_INCREMENT PRIMARY KEY, 
   status VARCHAR(45), created_at DATETIME, 
   updated_at DATETIME, 
   fk_customer INT REFERENCES customer (id_customer), 
   fk_product_subscriped_to INT REFERENCES product (id_product)
-  );
+);
 
 DROP TABLE IF EXISTS orders;
-CREATE TABLE orders (
+CREATE TABLE orders 
+(
   id_order INT AUTO_INCREMENT PRIMARY KEY, 
   order_number VARCHAR(45), 
   delivery_date DATE, 
@@ -21,32 +23,32 @@ CREATE TABLE orders (
   updated_at DATETIME, 
   fk_subscription INT REFERENCES subscription (id_subscription), 
   fk_product INT REFERENCES product (id_product)
-  );
+);
 
 
 DROP TABLE IF EXISTS product_family;
 CREATE TABLE product_family 
-  (
+(
     id_product_family INT AUTO_INCREMENT PRIMARY KEY, 
     product_family_handle VARCHAR(45) UNIQUE
-    );
+);
     
     
 DROP TABLE IF EXISTS product;
 
 CREATE TABLE product 
-	(
+(
     id_product INT AUTO_INCREMENT PRIMARY KEY, 
     product_sku VARCHAR(45) UNIQUE, 
     number_of_recipes TINYINT(1), 
     number_of_meals TINYINT(1), 
     fk_product_family INT REFERENCES product_family (id_product_family)
-    );
+);
     
     
 DROP TABLE IF EXISTS customer;
 CREATE TABLE customer 
-	(
+(
     id_customer INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(45),
     last_name VARCHAR(45),
@@ -54,7 +56,7 @@ CREATE TABLE customer
     password VARCHAR(45),
     created_at DATETIME,
     updated_at DATETIME
-    );
+);
     
 DROP TABLE IF EXISTS subscription;
 CREATE TABLE subscription
@@ -68,7 +70,7 @@ CREATE TABLE subscription
     );
 DROP TABLE IF EXISTS orders;
 CREATE TABLE orders 
-	(
+(
     id_order INT AUTO_INCREMENT PRIMARY KEY,
     order_number VARCHAR(45), 
     delivery_date DATE, 
@@ -77,7 +79,7 @@ CREATE TABLE orders
     updated_at DATETIME,
     fk_subscription INT REFERENCES subscription (id_subscription),
     fk_product INT REFERENCES product (id_product)
-	);
+);
     
     
 INSERT INTO product_family (product_family_handle) VALUES ('meat');
@@ -114,20 +116,20 @@ INSERT INTO orders (order_number, delivery_date, purchase_price, created_at, upd
 
 SELECT product_sku FROM product 
 INNER JOIN subscription 
-					ON subscription.id_subscription = product.id_product 
+ON subscription.id_subscription = product.id_product 
 INNER JOIN customer 
-					ON subscription.id_subscription = customer.id_customer 
-					WHERE email = 'JohnJSmith07074@aohel.com'
-					and status = 'active';
+ON subscription.id_subscription = customer.id_customer 
+WHERE email = 'JohnJSmith07074@aohel.com'
+and status = 'active';
 
 SELECT id_customer, first_name, last_name 
 FROM customer 
 INNER JOIN subscription 
-					ON customer.id_customer = subscription.id_subscription 
+	ON customer.id_customer = subscription.id_subscription 
 INNER JOIN product 
-					ON subscription.id_subscription = product.id_product 
+	ON subscription.id_subscription = product.id_product 
 INNER JOIN product_family 
-					ON product.fk_product_family = product_family.id_product_family 
+	ON product.fk_product_family = product_family.id_product_family 
 Where product_family_handle = 'classic-box';
 
 
@@ -135,44 +137,48 @@ Where product_family_handle = 'classic-box';
 SELECT status
 FROM subscription
 INNER JOIN orders
-			ON subscription.id_subscription = orders.id_order
+	ON subscription.id_subscription = orders.id_order
 WHERE STATUS = 'Paused' AND order_number = '1'; 
+
 
 
 SELECT AVG (id_subscription) FROM subscription WHERE status = 'active';
 
 SELECT COUNT (id_customer) AS customer_count FROM customer 
-INNER JOIN subscription ON customer.id_customer = subscription.id_subscription
-INNER JOIN product ON subscription.id_subscription = product.id_product 
-INNER JOIN orders ON product.id_product = orders.id_order WHERE COUNT (id_order) = ! '1';
+INNER JOIN subscription 
+	ON customer.id_customer = subscription.id_subscription
+INNER JOIN product 
+	ON subscription.id_subscription = product.id_product 
+INNER JOIN orders 
+	ON product.id_product = orders.id_order WHERE COUNT (id_order) = ! '1';
 
 
 SELECT COUNT (id_customer) AS customer_count 
 FROM customer 
 INNER JOIN subscription 
-					ON customer.id_customer = subscription.id_subscription
+	ON customer.id_customer = subscription.id_subscription
 INNER JOIN product 
-					ON subscription.id_subscription = product.id_product 
+	ON subscription.id_subscription = product.id_product 
 INNER JOIN orders 
-					ON product.id_product = orders.id_order WHERE COUNT (id_order) =! '1';
-
+	ON product.id_product = orders.id_order WHERE COUNT (id_order) =! '1';
 
 
 SELECT id_customer, first_name, last_name 
 FROM customer 
 INNER JOIN subscription 
-					ON customer.id_customer = subscription.fk_customer
+	ON customer.id_customer = subscription.fk_customer
 INNER JOIN orders
-					on subscription.fk_product_subscribed_to = orders.fk_subscription;
-                    
+	ON subscription.fk_product_subscribed_to = orders.fk_subscription;
+          
+	  
 SELECT COUNT(id_customer) AS customer_count
 FROM customer 
 INNER JOIN subscription 
-					ON customer.id_customer = subscription.fk_customer
+	ON customer.id_customer = subscription.fk_customer
 INNER JOIN product 
-					ON subscription.id_subscription = product.id_product
+	ON subscription.id_subscription = product.id_product
 INNER JOIN orders 
-					ON product.id_product = orders.id_order;
+	ON product.id_product = orders.id_order;
                     
                     
                     
